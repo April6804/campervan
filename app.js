a3203e3a2ceaf705014332a44505aff8cf42dd63
April Fultz <aprilfultz@gmail.com>
2019-05-04 11:57:37 -0400

    Hero-overlay positioning

--- app.js
+++ app.js
@@ -1,36 +1,11 @@
-// For lightbox on Gallery page--------------------------------
-$(".flexbox img").click(openModal);
-$(".close").click(closeModal);
-
-// Darken the page
-function openModal() {
-document.getElementById('myModal').style.display = "block";
-}
-
-// Remove the darkness
-function closeModal() {
-document.getElementById('myModal').style.display = "none";
-}
-
-
-function plusSlides(n) {
-showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-showSlides(slideIndex = n);
-}
-
-//Loop through slides, hide them, display one at a time
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+// Nav hamburger menu
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
-  slides[slideIndex-1].style.display = "block";
 }
 
 
